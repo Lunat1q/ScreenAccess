80b85a5b56243569c055d13687c7690c1270cafa
Lunat1q <grimfire@gmail.com>
2019-10-30 18:29:20 +0300

    Squashed commit of the following:
    commit fd85da2b9bcd4b521b50c5daa6ba5617e1918385
    Author: Lunat1q <grimfire@gmail.com>
    Date:   Wed Oct 30 18:25:08 2019 +0300
    
        - build script
    
    commit f3fb12803e8b488868d4b292c54014e93559aa5a
    Merge: 3719b30 a0b199c
    Author: Lunat1q <grimfire@gmail.com>
    Date:   Wed Oct 30 18:23:36 2019 +0300
    
        Merge branch 'one-liner-build' of https://github.com/Lunat1q/ScreenAccess into one-liner-build
    
        # Conflicts:
        #	GetAll.ps1
    
    commit 3719b307447efc7d36e2f535a76d3d303947ad13
    Author: Lunat1q <grimfire@gmail.com>
    Date:   Wed Oct 30 18:22:34 2019 +0300
    
        - remove 4.5 devpack
    
        # Conflicts:
        #	GetAll.ps1
    
    commit ef6aa1b041325a29d421de1513d4e4a631614bdc
    Author: Max <grimfire@gmail.com>
    Date:   Wed Oct 30 16:40:05 2019 +0300
    
        - test
    
    commit 61aa01aa0f2b19ebe75a43d0b442b0eef2f416d6
    Author: Max <grimfire@gmail.com>
    Date:   Wed Oct 30 16:36:24 2019 +0300
    
        - prevent duplicate clone
    
    commit 2314e6589b64682b023ed463dbd00621e898f502
    Author: Max <grimfire@gmail.com>
    Date:   Wed Oct 30 16:34:47 2019 +0300
    
        - temp script route
    
    commit a0b199cafe659c0f0f443a6f71b7e3a7fc9ffcf5
    Author: Lunat1q <grimfire@gmail.com>
    Date:   Wed Oct 30 18:18:36 2019 +0300
    
        - temp script route
    
    commit b0792e180f9be9f88e08d4c330de0f659064eea8
    Author: Max <grimfire@gmail.com>
    Date:   Sun Oct 27 16:51:09 2019 +0300
    
        - fix auto-open

--- buildMe.ps1
+++ buildMe.ps1
@@ -1,21 +1,38 @@
+function Test-RegistryValue($path, $name)
+{
+    $key = Get-Item -LiteralPath $path -ErrorAction SilentlyContinue
+    $key -and $null -ne $key.GetValue($name, $null)
+}
+
+git pull
+
 $vsPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 if (!(Test-Path $vsPath))
 {
 	Write-Host "Visual Studio 2019 not found, looking for VS2017"
 	$vsRegPath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7"
-	$vs17Path = Get-ItemPropertyValue -Path $vsRegPath -Name "15.0"
-	$vs17Path = [System.IO.Path]::Combine($vs17Path, "MSBuild\15.0\Bin\amd64\MSBuild.exe");
+	$vs17Path = $null
+	if ((Test-RegistryValue $vsRegPath "15.0"))
+	{
+		$vs17Path = Get-ItemPropertyValue -Path $vsRegPath -Name "15.0"
+		$vs17Path = [System.IO.Path]::Combine($vs17Path, "MSBuild\15.0\Bin\amd64\MSBuild.exe");
+	}
 	
-	if (Test-Path $vs17Path)
+	if ($vs17Path -and (Test-Path $vs17Path))
 	{
 		$vsPath = $vs17Path
 	}
 	else
 	{
 		$vsToolsPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
+		$vsToolsPath32 = "C:\Program Files\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
 		if (Test-Path $vsToolsPath)
 		{
 			$vsPath = $vsToolsPath
+		}
+		elseif (Test-Path $vsToolsPath32)
+		{
+			$vsPath = $vsToolsPath32
 		}
 		else
 		{
@@ -27,5 +44,12 @@ if (!(Test-Path $vsPath))
 
 nuget restore
 
-Start-Process -FilePath $vsPath -ArgumentList "/Restore /t:Build /p:Configuration=Release build.proj" -Wait
-explorer .\TiqLauncher.ScreenAssistant\bin\Release
+$restoreProc = Start-Process -FilePath $vsPath -ArgumentList "/Restore /p:Configuration=Release build.proj" -PassThru
+
+$restoreProc | Wait-Process -Timeout 60 -ErrorAction SilentlyContinue
+$buildProc = Start-Process -FilePath $vsPath -ArgumentList "/t:Build /p:Configuration=Release build.proj" -PassThru
+$buildProc | Wait-Process -Timeout 120 -ErrorAction SilentlyContinue
+if (Test-Path "TiqLauncher.ScreenAssistant\bin\Release")
+{
+	explorer .\TiqLauncher.ScreenAssistant\bin\Release
+}
