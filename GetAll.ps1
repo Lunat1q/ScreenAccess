20dc21bc0539a810d8c1ffec78e17be498659c06
Max <grimfire@gmail.com>
2019-10-30 16:33:52 +0300

    - script changes

--- GetAll.ps1
+++ GetAll.ps1
@@ -8,8 +8,12 @@ if (Test-Path ".git")
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install git.install -y
-choco install visualstudio2019buildtools -y
+choco install visualstudio2019buildtools -y --package-parameters "--includeRecommended --passive --locale en-US"
 choco install nuget.commandline -y
+choco install dotnetcore-sdk -y
+choco install netfx-4.5-devpack -y
+choco install netfx-4.5.2-devpack -y
+choco install netfx-4.6.2-devpack -y
 
 if (!($cloned))
 {
