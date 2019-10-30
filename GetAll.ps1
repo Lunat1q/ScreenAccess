5dd3c584e0cef3821bb3f95e1f3c9f3f58699617
Max <grimfire@gmail.com>
2019-10-30 16:36:24 +0300

    - prevent duplicate clone

--- GetAll.ps1
+++ GetAll.ps1
@@ -17,8 +17,15 @@ choco install netfx-4.6.2-devpack -y
 
 if (!($cloned))
 {
-	git clone "https://github.com/Lunat1q/ScreenAccess" -b "weapons_test"
-	cd "ScreenAccess"
+	if (Test-Path "ScreenAccess")
+	{
+		cd "ScreenAccess"
+	}
+	else
+	{
+		git clone "https://github.com/Lunat1q/ScreenAccess" -b "weapons_test"
+		cd "ScreenAccess"
+	}
 }
 
 git submodule init
